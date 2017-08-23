df0281947f623a3a1cce18f2ec87110c1212a683
Samanta <samanta-07@hotmail.com>
2017-08-23 11:05:15 +0200

    added ConnectionLost

---
+++ src/NetworkLib/ConnectionLostEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NetworkLib
+{
+    public class ConnectionLostEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConnectionLostEventArgs"/> class.
+        /// </summary>
+        /// <param name="message">Contains the lost connection info message.</param>
+        public ConnectionLostEventArgs(string message = "")
+        {
+            this.Message = message;
+        }
+
+        /// <summary>
+        /// Gets the info message.
+        /// </summary>
+        /// <value>
+        ///     Contains the reason for the lost connection.
+        /// </value>
+        public string Message { get; private set; }
+    }
+}
